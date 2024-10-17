b017b7af627c334673e3bbe643faf5a8539d14a9
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-17 12:44:35 +0700

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -10,7 +10,18 @@ private:
      string ID, position, salary;
 public:
      Staff ();
-     Staff (string );
+     Staff (const Staff &s) : Person (s), ID(s.ID), position(s.position), salary (s.salary) {}
+     Staff (string fullName, string CCCD, string phone, string add, string gender, Date DOB, string ID, string position, string salary) 
+                                        : Person (fullName, CCCD, phone, add, gender, DOB), ID (ID), position(position), salary (salary) {}
+
+     void setID (string ID);
+     string getID () const;
+     bool setPosition (string position);
+     string getPosition () const;
+     void setSalary (string salary);
+     string getSalary () const;
+
+     ~ Staff () {}
 };
 
 #endif
