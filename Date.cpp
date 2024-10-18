a8a7466bfa5faf12bc7aad0be3ba5984662d6c62
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-18 16:33:59 +0700

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -119,11 +119,3 @@ void Date::display() const {
     }
     cout << dayStr + "/" + monthStr + "/" + yearStr << endl;
 }
-
-ostream& operator << (ostream &os, const Date &date)
-{
-    os << (date.day < 10 ? "0" : "") << date.day << "/"
-       << (date.month < 10 ? "0" : "") << date.month << "/"
-       << date.year;
-    return os;
-}
