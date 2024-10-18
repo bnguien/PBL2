18dbb0a05038ab63cfc62b8c51239ccb62b64db4
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-18 17:06:00 +0700

    Create Staff_test.cpp

---
+++ Staff_test.cpp
@@ -0,0 +1,9 @@
+#include "Staff.cpp"
+
+int main ()
+{
+     string fileStaff = "Staff.txt";
+     vector<Staff> staffs = readFileStaff (fileStaff);
+     Staff staff;
+     staff.displayStaff (staffs);
+}
