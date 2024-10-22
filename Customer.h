62c1b76e1d12547eadb411b1ff6af9ab09f6e150
bnguien <nguyennguyendangbao8@gmail.com>
2024-10-22 19:53:37 +0700

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -2,6 +2,8 @@
 #define CUSTOMER_H
 #include "Person.h"
 #include "Date.h"
+#include "Service.h"
+#include "Room.h"
 #include<iostream>
 #include<vector>
 #include<string>
@@ -11,24 +13,27 @@ class Customer : public Person
 {
     private: 
         vector<string> roomIDs; //Danh sách ID phòng
+        vector<string> serviceIDs; //Danh sách dịch vụ
         Date arrivalDate; //Ngày đến
+        bool checkedOut;   
     public:
         Customer(): Person() {} // Default constructor
-        Customer(const Customer &g) : Person(g) 
-            {
-                this->roomIDs = g.roomIDs;
-                this->arrivalDate = g.arrivalDate; 
-            }
+        Customer(const Customer &g) : Person(g), roomIDs(g.roomIDs), 
+                                      arrivalDate(g.arrivalDate), checkedOut(g.checkedOut) {}
          Customer(const Person& person, const vector<string>& roomIDs, const Date& arrivalDate) 
-        : Person(person), roomIDs(roomIDs), arrivalDate(arrivalDate) {}
-        void displayCustomer(const vector<Customer>& customers);
+        : Person(person), roomIDs(roomIDs), arrivalDate(arrivalDate), checkedOut(false) {}
+        void displayCustomer(const vector<Customer>& customers, const vector<Service>& services);
         void setArrivalDate(const Date& arrivalDate);
         Date getArrivalDate () const;
         vector<string> getRoomIDs() const;
         void setRoomIDs(const vector<string>& roomIDs);
+        bool getCheckedOut() const;
+        void setCheckedOut(bool status);
         static vector<Customer> readFileCustomer(const string& fileName);
         void bookedRoom();
-        void checkInfor(const string& inputUserName, const vector<Customer>& customers);
+        void bookService();
+        void checkInfor(const string& inputUserName, const vector<Customer>& customers, const vector<Service>& services);
+        void checkout(const string& inputUserName, const vector<Customer>& customers);
         ~Customer();
 };
 #endif
