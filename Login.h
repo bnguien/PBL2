f3053ec6bbd729bfda93c85b2284b984ec48d671
bnguien <nguyennguyendangbao8@gmail.com>
2024-10-22 14:19:47 +0700

    Create Login.h

---
+++ Login.h
@@ -0,0 +1,21 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+void setColor(int color);
+void loadingBarAnimation();
+static inline string trim(string s);
+string toLower(string s);
+string createUsername(const string& fullName);
+string inputPassword();
+bool login(const vector<pair<string, string>>& accounts, const string& username, const string& password);
+
+#endif
