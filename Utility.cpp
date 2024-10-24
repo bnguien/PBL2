113f0765213c6f81845106e5319ef53fc5151acb
bnguien <nguyennguyendangbao8@gmail.com>
2024-10-24 11:31:41 +0700

    Delete Utility.cpp

--- Utility.cpp
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-#include "utility.h"
-
-void changeConsoleColor(int colorCode) {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, colorCode);
-}
