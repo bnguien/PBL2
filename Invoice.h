aa538f76a137718e2ef00c4e7772f7ec238e0767
bnguien <nguyennguyendangbao8@gmail.com>
2024-10-27 21:13:49 +0700

    Update Invoice.h

--- Invoice.h
+++ Invoice.h
@@ -2,30 +2,39 @@
 #define INVOICE_H
 
 #include "Function.h"
+#include "Vector.h"
+#include "Customer.h"
+#include "Room.h"
+#include "Service.h"
+#include "Date.h"
+
 using namespace std;
 
 class Invoice 
 {
-    private:
-        Customer customer;
-        string InvoiceID;
-        string PayMentStt, PayMentMethod;
-        vector<string> roomIDs; 
-        vector<string> services;
-        Date checkinDate, checkoutDate;
-        double totalPrice;
-    public:
-        Invoice(){};
-        Invoice(const Customer& cust, const vector<string>& roomIDs, const Date& checkin)
-        : customer(cust), roomIDs(roomIDs), checkinDate(checkin), totalPrice(0.0) 
+private:
+    Customer customer;          
+    string invoiceID;          
+    string paymentStatus;       
+    string paymentMethod;     
+    vector<string> roomIDs;    
+    vector<string> services;     
+    Date checkinDate;     
+    Date checkoutDate;        
+    double totalPrice;      
+
+public:
+    Invoice() : totalPrice(0.0) {}
+    Invoice(const Customer& cust, const vector<string>& roomIDs, const Date& checkin, const Date& checkout)
+        : customer(cust), roomIDs(roomIDs), checkinDate(checkin), checkoutDate(checkout), totalPrice(0.0)
     {
-        checkoutDate = checkoutDate.getCurrentDate();
+        invoiceID = createID(customer);
     }
-        string createID(const Customer& customer);
-        void calculateTotalPrice(const string& roomFile, const string& serviceFile);
-        void setPaymentMethod(const string& method);
-        void printInvoice();
-        ~Invoice();
-        
+    string createID(const Customer& customer);
+    void calculateTotalPrice(const string& roomFile, const string& serviceFile);
+    void setPaymentMethod(const string& method);
+    void printInvoice() const;
+    ~Invoice() {}
 };
+
 #endif
