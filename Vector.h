58180e16bc554233791257577fb24e211af9a130
bnguien <nguyennguyendangbao8@gmail.com>
2024-10-27 22:27:30 +0700

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,9 +1,9 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-#include "Function.h"
-
-using namespace std;
+#include <initializer_list>
+#include <stdexcept>
+#include <algorithm>
 
 template <typename T>
 class vector
@@ -13,11 +13,13 @@ private:
     size_t size_;
     size_t capacity_;
 
+    void resize(size_t newCapacity);
+
 public:
     vector();
     vector(const T *array, size_t n);
-    vector(size_t n, const T &value = T());
-    vector(initializer_list<T> list);
+    vector(size_t n, const T &value);
+    vector(std::initializer_list<T> list);
     ~vector();
 
     size_t size() const;
@@ -25,16 +27,18 @@ public:
     bool empty() const;
     void push_back(const T &value);
     void pop_back();
-    T &get(size_t index);
-    const T &get(size_t index) const;
-    void set(size_t index, const T &value);
-    void clear();
-
     T &operator[](size_t index);
     const T &operator[](size_t index) const;
+    T &get(size_t index);
+    const T &get(size_t index) const;
 
-    T *begin() const;
-    T *end() const;
+    T *begin();
+    T *end();
+    const T *begin() const;
+    const T *end() const;
+    void clear();
 };
+
+#include "Vector.cpp"
 
 #endif
