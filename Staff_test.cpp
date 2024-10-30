9766a43148f6983420727660b86f822d6e5ec3f5
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-30 23:55:30 +0700

    Update Staff_test.cpp

--- Staff_test.cpp
+++ Staff_test.cpp
@@ -1,9 +1,10 @@
 #include "Staff.cpp"
+#include "Date.cpp"
+#include "Person.cpp"
 
-/*int main ()
+int main ()
 {
-     string fileStaff = "Staff.txt";
-     vector<Staff> staffs = readFileStaff (fileStaff);
-     Staff staff;
-     staff.displayStaff (staffs);
-}*/
+     Date date(10,10,2006);
+     Staff staff("Ngan Nguyen", "047389251238", "0384698955", "Quang Tri", "Nu", date, "L05", "Laundry", "4.000.000/month");
+     staff.saveStafftoStaffFile(staff, "Staff.txt");
+}
