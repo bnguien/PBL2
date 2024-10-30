750e8ced40f1139b2d17d9262dd35ed42a1141ca
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-30 16:09:53 +0700

    Update Person.h

--- Person.h
+++ Person.h
@@ -4,47 +4,50 @@
 #include "Date.h"
 #include "Vector.h"
 
-using namespace std; 
+using namespace std;
 
-class Person {
+class Person
+{
 private:
-     string fullName;
-     string CCCD;
-     string phone;
-     string add;
-     string gender;
-     Date DOB;
+    string fullName;
+    string CCCD;
+    string phone;
+    string add;
+    string gender;
+    Date DOB;
+
 public:
-     Person () {}
-     Person (const Person &P):fullName(P.fullName), CCCD(P.CCCD), phone(P.phone), add(P.add), gender(P.gender), DOB(P.DOB){}
-     Person (string fullName, string CCCD, string phone, string add, string gender, Date DOB) {
-          this->fullName = fullName;
-          this->CCCD = CCCD;
-          this->phone = phone;
-          this->add = add;
-          this->gender=gender;
-          this->DOB = DOB;
-     }
+    Person() {}
+    Person(const Person &P) : fullName(P.fullName), CCCD(P.CCCD), phone(P.phone), add(P.add), gender(P.gender), DOB(P.DOB) {}
+    Person(string fullName, string CCCD, string phone, string add, string gender, Date DOB)
+    {
+        this->fullName = fullName;
+        this->CCCD = CCCD;
+        this->phone = phone;
+        this->add = add;
+        this->gender = gender;
+        this->DOB = DOB;
+    }
 
-     bool setFullName (const string& fullName);
-     string getFullName () const;
-     bool setCCCD (const string& CCCD);
-     string getCCCD () const;
-     bool setPhone (const string& phone);
-     string getPhone () const;
-     void setAdd (const string& add);
-     string getAdd () const;
-     void setGender (const string& gender);
-     string getGender() const;
-     bool setDOB (const Date& DOB);
-     Date getDOB () const;
+    bool setFullName(const string &fullName);
+    string getFullName() const;
+    bool setCCCD(const string &CCCD);
+    string getCCCD() const;
+    bool setPhone(const string &phone);
+    string getPhone() const;
+    void setAdd(const string &add);
+    string getAdd() const;
+    void setGender(const string &gender);
+    string getGender() const;
+    bool setDOB(const Date &DOB);
+    Date getDOB() const;
 
-     void splitName (const string &fullName, string &name);
-     void toLowerCase (string &str);
-     string standardizeString (const string& input);
-     static vector<Person> readFilePerson(const string& fileName);
-     static void displayPerson(const vector<Person>& people);
-     ~ Person() {}
+    void splitName(const string &fullName, string &name);
+    void toLowerCase(string &str);
+    string standardizeString(const string &input);
+    static vector<Person> readFilePerson(const string &fileName);
+    static void displayPerson(const vector<Person> &people);
+    ~Person() {}
 };
 
 #endif
