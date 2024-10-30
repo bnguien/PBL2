e047c70a166288bdf96a13527daf9ff67aeb20d5
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-30 23:55:26 +0700

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -2,29 +2,36 @@
 #define STAFF_H
 
 #include "Person.h"
-#include "Function.h"
 
 class Staff : public Person
 {
 private:
      string ID, position, salary;
+
 public:
-     Staff () {}
-     Staff (const Staff &s) : Person (s), ID(s.ID), position(s.position), salary (s.salary) {}
-     Staff (string fullName, string CCCD, string phone, string add, string gender, Date DOB, string ID, string position, string salary) 
-                                        : Person (fullName, CCCD, phone, add, gender, DOB), ID (ID), position(position), salary (salary) {}
+     Staff() {}
+     Staff(const Staff &s) : Person(s), ID(s.ID), position(s.position), salary(s.salary) {}
+     Staff(string fullName, string CCCD, string phone, string add, string gender, Date DOB, string ID, string position, string salary)
+         : Person(fullName, CCCD, phone, add, gender, DOB), ID(ID), position(position), salary(salary) {}
 
-     void setID (const string& ID);
-     string getID () const;
-     bool setPosition (const string& position);
-     string getPosition () const;
-     void setSalary (const string& salary);
-     string getSalary () const;
+     void setID(const string &ID);
+     string getID() const;
+     bool setPosition(const string &position);
+     string getPosition() const;
+     void setSalary(const string &salary);
+     string getSalary() const;
 
-     static vector<Staff> readFileStaff (const string &fileName);
-     void displayStaff (const vector<Staff>& staffs);
+     bool operator == (const Staff& staff) const; 
+     static vector<Staff> readFileStaff(const string &fileName);
+     void displayStaff(const vector<Staff> &staffs);
 
-     ~ Staff () {}
+     void updateStaffFile(const vector<Staff> &staffs, const string &fileName);
+     void addNewStaff(const Staff &newStaff);
+     void removeStaff(Staff &staffToRemove);
+     void removeStaffByCCCD(const string &CCCDToRemove);
+     ~Staff() {}
 };
+
+
 
 #endif
