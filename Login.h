d4f9c5353814eef334c20cc09eacb4694c45bc9c
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-30 16:32:54 +0700

    Update Login.h

--- Login.h
+++ Login.h
@@ -1,13 +1,13 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
+#include "Vector.h"
 using namespace std;
 
 string toLower(string s);
 string createUsername(const string& fullName);
 string inputPassword();
 bool login(const vector<pair<string, string>>& accounts, const string& username, const string& password);
-void changeConsoleColor(int colorCode=7);
 void loadingBarAnimation(int color=7);
 string trim(string s);
 
