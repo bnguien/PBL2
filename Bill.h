3fe31f31ce478bacbef8d8e045908cbf2a78509c
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-10-30 16:05:45 +0700

    Update Bill.h

--- Bill.h
+++ Bill.h
@@ -1,41 +1,39 @@
 #ifndef BILL_H
 #define BILL_H
 
-#include "Function.h"
-#include "Vector.h"
-#include "Room.h"
 #include "Service.h"
-#include "Date.h"
+#include "Room.h"
+class Date;
 
 using namespace std;
 
-class Bill 
+class Bill
 {
 protected:
-    Customer customer;          
-    string BillID;          
-    string paymentStatus;       
-    string paymentMethod;     
-    vector<string> roomIDs;    
-    vector<string> services;     
-    Date checkinDate;     
-    Date checkoutDate;        
-    double totalPrice;      
+    Customer customer;
+    string BillID;
+    string paymentStatus;
+    string paymentMethod;
+    vector<string> roomIDs;
+    vector<string> services;
+    Date checkinDate;
+    Date checkoutDate;
+    double totalPrice;
 
 public:
     Bill();
-   Bill::Bill(const Customer& cust, const vector<string>& roomIDs, const Date& checkout)
-    : customer(cust), roomIDs(roomIDs), checkinDate(cust.getArrivalDate()), checkoutDate(checkout), totalPrice(0.0) 
-{
-    BillID = createID(customer);
-}
+    Bill::Bill(const Customer &cust, const vector<string> &roomIDs, const Date &checkout)
+        : customer(cust), roomIDs(roomIDs), checkinDate(cust.getArrivalDate()), checkoutDate(checkout), totalPrice(0.0)
+    {
+        BillID = createID(customer);
+    }
 
     Date getCheckoutDate() const;
-    void setCheckoutDate(const Date& date) ;
-    virtual int convert(const string& input);
-    string createID(const Customer& customer);
-    void calculateTotalPrice(const vector<Customer> &customers,const vector<Room> &rooms, const vector<Service> &services) ;
-    void setPaymentMethod(const string& method);
+    void setCheckoutDate(const Date &date);
+    virtual int convert(const string &input);
+    string createID(const Customer &customer);
+    void calculateTotalPrice(const vector<Customer> &customers, const vector<Room> &rooms, const vector<Service> &services);
+    void setPaymentMethod(const string &method);
     virtual void displayBill() const;
 
     void inputCheckoutDate();
