9961b7f7a18656b5f4485389963e87dd8d099def
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-11-01 00:21:10 +0700

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -2,6 +2,9 @@
 #define STAFF_H
 
 #include "Person.h"
+class Customer;
+class Room;
+class Service;
 
 class Staff : public Person
 {
@@ -21,17 +24,28 @@ public:
      void setSalary(const string &salary);
      string getSalary() const;
 
-     bool operator == (const Staff& staff) const; 
+     bool operator ==(const Staff& staff) const; 
      static vector<Staff> readFileStaff(const string &fileName);
+     static string generateStaffID(const vector<Staff> &staffs, const string &position);
      void displayStaff(const vector<Staff> &staffs);
 
      void updateStaffFile(const vector<Staff> &staffs, const string &fileName);
-     void addNewStaff(const Staff &newStaff);
+     void addNewStaff(Staff &newStaff);
      void removeStaff(Staff &staffToRemove);
      void removeStaffByCCCD(const string &CCCDToRemove);
+     
+     void updateCustomerFile(const vector<Staff> &staffs, const string &fileName);
+     void addNewCustomer(Customer &newCus);
+     void removeCustomer(Customer &CusToRemove);
+     void removeCustomerByCCCD(const string &CCCDToRemove);
+
+     bool changeRoomStatus(const string &roomID); //true: doi thanh cong, false: gap loi
+
+     void updateServiceFile(const vector<Service> &services, const string &fileName);
+     void addNewService(Service &newService);
+     void removeService(const string ID);
      ~Staff() {}
 };
-
 
 
 #endif
