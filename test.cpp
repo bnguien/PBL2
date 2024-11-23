ad6b21c2403a83abd06f391c4de92dda3465ba47
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-11-23 21:55:48 +0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include "Room.cpp"
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+     vector<Room> rooms = Room::readFileRoom("Room.h");
+     string border = "+-----+--------+-----------+------------------------+-------------+-----------------------------------+";
+     
+}
