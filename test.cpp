7fab6e79c9019f04a493e9e895ef28d7a0463983
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2024-11-26 00:28:37 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,39 +1,54 @@
 #include "Room.cpp"
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
-int main ()
+int main()
 {
-     vector<Room> rooms = Room::readFileRoom("Room.txt");
-     string border1 = "+-----+--------+----------+---------------+-------------+-------------------------------------+";
-     string border2 = "|     |        |          |               |             | ";
+     string fileRoom = "Room.txt";
+     vector<Room> rooms = Room::readFileRoom(fileRoom);
+     string border1 = "+-----+--------+----------+---------------+--------------+-------------------------------------+";
+     string border2 = "|     |        |          |               |              | ";
      cout << border1 << endl;
-     cout << "| STT | RoomID | RoomType |   RoomPrice   |    Status    |             Service IDs             |";
+     cout << "| STT | RoomID | RoomType |   RoomPrice   |    Status    |             Service IDs             |" << endl;
      int count = 0;
      for (auto room : rooms)
      {
-          ++ count;
+          ++count;
           cout << "| " << setw(3) << setfill('0') << count << " |  "
-               << setw(6) << room.getID() << "|  " 
+               << setw(6) << room.getID() << "|  "
                << setw(8) << room.getType() << "| "
                << setw(14) << room.getPrice() << "| "
                << setw(13) << room.getStatus() << "| ";
-          string serID = "";
-          int index = 0;
-          while (index < room.getServiceIDs().size() - 1)
+
+          vector<string> serviceIDs = room.getServiceIDs();
+
+          if (serviceIDs.empty())
           {
-               serID += room.getServiceIDs()[index] + ", ";
-               ++ index;
-               if (serID.length() == 37)
-               {
-                    serID.erase(serID.length() - 2);
-                    cout << serID << " |" << endl;
-                    cout << border2;
-                    serID = "";
-               }
+               cout << setw(36) << left << "None" << "|" << endl;
           }
-          serID += room.getServiceIDs()[index + 1];
-          cout << setw(36) << serID << "|" << endl;
+          else
+          {
+               string serID = "";
+               int index = 0;
+               while (index < serviceIDs.size())
+               {
+                    serID += serviceIDs[index];
+                    ++index;
+                    if (index < serviceIDs.size())
+                         serID += ", ";
+
+                    if (serID.length() >= 37 || index == serviceIDs.size())
+                    {
+                         cout << setw(36) << left << serID << "|" << endl;
+                         if (index < serviceIDs.size())
+                              cout << border2 << endl;
+                         serID = "";
+                    }
+               }
+               cout << border1 << endl;
+          }
      }
+     return 0;
 }
