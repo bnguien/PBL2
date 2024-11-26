722af2db02f5b910ed0f2e367798a81b2252214f
bnguien <nguyennguyendangbao8@gmail.com>
2024-11-26 10:00:52 +0700

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -54,7 +54,7 @@ public:
      int cusExists(const vector<Customer> &customers, const Customer &newCus);
      bool writeRemainingCus(const vector<Customer> &remainingCustomers, const string &fileName);
      bool addNewCustomer(Staff &staff);
-     bool updateCustomer(const string &fileName, vector<Customer> &customers);
+     bool updateCustomer (Staff &staff, const string &fileName, vector<Customer> &customers);
      bool removeCustomerByCCCD(const Staff &staff,const string &CCCDToRemove);
 
      bool findCustomerByCCCD(const Staff &staff, const string &CCCD);
